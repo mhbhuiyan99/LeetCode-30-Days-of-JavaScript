c9feb17b3c138f908f3eb87ba82bb647b3859ec5
Mojammel Haque Bhuiyan <98250129+mhbhuiyan99@users.noreply.github.com>
2025-02-03 10:21:43 +0600

    Create Day - 4.js

---
+++ Day - 4.js
@@ -0,0 +1,23 @@
+var createCounter = function(init) {
+    
+    let x = init;
+    return {
+        increment(){
+            return ++init;
+        },
+        decrement(){
+            return --init;
+        },
+        reset(){
+            init = x;
+            return init;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
