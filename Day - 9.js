4efe1a31e329f1083c6e50e60bf3184cfc201dd3
Mojammel Haque Bhuiyan <98250129+mhbhuiyan99@users.noreply.github.com>
2025-11-03 10:29:59 +0600

    Add function to return length of arguments
    Implement a function to return the length of arguments passed.

---
+++ Day - 9.js
@@ -0,0 +1,5 @@
+// Problem Link: https://leetcode.com/problems/return-length-of-arguments-passed/?envType=study-plan-v2&envId=30-days-of-javascript
+
+var argumentsLength = function(...args) {
+    return args.length 
+};
